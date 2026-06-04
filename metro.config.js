fc14de2d73072f03cf4a54e9ae42c4607b1032f7
Chamso Houa <98857659+chamsohoua@users.noreply.github.com>
2026-06-04 12:58:55 +0100

    Delete metro.config.js

--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver.sourceExts.push('mjs', 'cjs');
-
-config.resolver.assetExts.push('glb', 'gltf', 'bin', 'hdr');
-
-module.exports = config;
